aa71c31abb8616130ecedb27f73ab383e6d96d3b
Micrologist <mail@davidgalland.de>
2020-04-03 09:27:57 +0200

    rewrite with bethesda support and extended perf metr output

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,9 +15,23 @@ namespace DoomTrainer
 	{
 
 		globalKeyboardHook hook = new globalKeyboardHook();
-		SigScanTarget consoleCmdScanTarget = new SigScanTarget("4C8B0EBA01000000488BCE448BF041FF51??8D");
-		SigScanTarget consoleBindScanTarget = new SigScanTarget("4C8B0FBA01000000488BCF448BF041FF51??4C6BC507");
+		Process process;
+		public bool hooked = false;
 
+		DeepPointer raxDP;
+		DeepPointer eaxDP;
+		DeepPointer velDP;
+		DeepPointer rotDP;
+		DeepPointer yawDP;
+		DeepPointer row1DP;
+		DeepPointer row2DP;
+		DeepPointer row3DP;
+		DeepPointer row4DP;
+		DeepPointer row5DP;
+		DeepPointer row6DP;
+		DeepPointer row7DP;
+		DeepPointer row8DP;
+		DeepPointer row9DP;
 
 		IntPtr xLocPtr;
 		IntPtr yLocPtr;
@@ -29,27 +43,27 @@ namespace DoomTrainer
 		IntPtr yRotPtr;
 		IntPtr xYawPtr;
 		IntPtr yYawPtr;
-		DeepPointer raxPointer = new DeepPointer("DOOMEternalx64vk.exe", 0x06121BB8, 0x38, 0x28, 0x0);
-		DeepPointer eaxPointer = new DeepPointer("DOOMEternalx64vk.exe", 0x04C7CA08, 0xCB0, 0xDF8, 0x1D0, 0x88);
-		DeepPointer xVelDP = new DeepPointer("DOOMEternalx64vk.exe", 0x04C7CA08, 0x1510, 0x598, 0x1D0,  0x3F40);
-		DeepPointer rotDP = new DeepPointer("DOOMEternalx64vk.exe", 0x4C83F38);
-		DeepPointer yawDP = new DeepPointer("DOOMEternalx64vk.exe", 0x61AC728);
+		IntPtr row1ptr;
+		IntPtr row2ptr;
+		IntPtr row3ptr;
+		IntPtr row4ptr;
+		IntPtr row5ptr;
+		IntPtr row6ptr;
+		IntPtr row7ptr;
+		IntPtr row8ptr;
+		IntPtr row9ptr;
 
-		//DeepPointer frameTimeDebugDP = new DeepPointer("DOOMEternalx64vk.exe", 0x25B4A88);
-		DeepPointer frameTimeDebugDP = new DeepPointer("DOOMEternalx64vk.exe", 0x25B4A80);
-
-		IntPtr frameTimeDebugPTR;
+		float curX;
+		float curY;
+		float curZ;
+		float curXv;
+		float curYv;
+		float curZv;
+		double hVel;
 
 		float[] storedPos = new float[3] { 0f, 0f, 0f };
 		float[] storedVel = new float[3] { 0f, 0f, 0f };
 		float[] storedRot = new float[4] { 0f, 0f, 0f, 0f };
-		Process process;
-
-
-		DeepPointer restrictedCommandsDPTR = new DeepPointer("DOOMEternalx64vk.exe", 0x442741);
-		DeepPointer restrictedKeyPressDPTR = new DeepPointer("DOOMEternalx64vk.exe", 0x44FCE7);
-		IntPtr restrictedCommandsPtr;
-		IntPtr restrictedKeyPressPtr;
 
 		bool retainVel;
 
@@ -74,46 +88,33 @@ namespace DoomTrainer
 
 		public void updateTick(object snder, EventArgs e){
 
-			
-			if (!GetPointers())
-				return;
-			if (process.HasExited)
+			if (process == null || process.HasExited)
 			{
 				process = null;
-				return;
+				hooked = false;
 			}
+
+			if (!hooked)
+				hooked = Hook();
+
+			if (!hooked)
+				return;
+
+			DerefPointers();
+
 			
-
-			float curX;
 			process.ReadValue<float>(xLocPtr, out curX);
-			float curY;
 			process.ReadValue<float>(yLocPtr, out curY);
-			float curZ;
 			process.ReadValue<float>(zLocPtr, out curZ);
-
-			float curXv;
 			process.ReadValue<float>(xVelPtr, out curXv);
-			float curYv;
 			process.ReadValue<float>(yVelPtr, out curYv);
-			float curZv;
 			process.ReadValue<float>(zVelPtr, out curZv);
-
-			double hVel = Math.Sqrt(curXv * curXv + curYv * curYv);
-
-			unlockButton.IsEnabled = restrictedCommandsDPTR.DerefBytes(process, 1)[0] > 0;
+			hVel = Math.Sqrt(curXv * curXv + curYv * curYv);
 
 			PosBlock.Text = "Current Position\nX: " + curX.ToString("0.00") + "\nY: " + curY.ToString("0.00") + "\nZ: " + curZ.ToString("0.00") + "\n\n\nStored Position\nX: " + storedPos[0].ToString("0.00") + "\nY: " + storedPos[1].ToString("0.00") + "\nZ: " + storedPos[2].ToString("0.00");
 			VelBlock.Text = "Current Velocity\nX: " + curXv.ToString("0.00") + "\nY: " + curYv.ToString("0.00") + "\nZ: " + curZv.ToString("0.00") + "\n"+hVel.ToString("0.00")+"m/s\n\nStored Velocity\nX: " + storedVel[0].ToString("0.00") + "\nY: " + storedVel[1].ToString("0.00") + "\nZ: " + storedVel[2].ToString("0.00");
 
-
-			byte[] velocityArray = Encoding.ASCII.GetBytes("hvel: " + hVel.ToString("0.00") + " m/s | %i FPS");
-			byte[] outputArray = new byte[32];
-			for (int i = 0; i < velocityArray.Length; i++)
-			{
-				outputArray[i] = velocityArray[i];
-			}
-			process.VirtualProtect(frameTimeDebugPTR, 32, MemPageProtect.PAGE_EXECUTE_READWRITE);
-			process.WriteBytes(frameTimeDebugPTR, outputArray);
+			ShowPos();
 		}
 
 
@@ -124,7 +125,7 @@ namespace DoomTrainer
 
 		private void Hook_KeyDown(object sender, System.Windows.Forms.KeyEventArgs e)
 		{
-			if (GetPointers())
+			if (hooked)
 			{
 				if (e.KeyCode == System.Windows.Forms.Keys.F5)
 				{
@@ -167,9 +168,7 @@ namespace DoomTrainer
 					Console.WriteLine("yoffset: " + yOffset.ToString());
 
 					process.WriteBytes(xRotPtr, BitConverter.GetBytes(storedRot[0] + xOffset));
-					//process.WriteBytes(yRotPtr, BitConverter.GetBytes(storedRot[1]));
 					process.WriteBytes(yRotPtr, BitConverter.GetBytes(storedRot[1] + yOffset));
-					//process.WriteBytes(yawPtr, BitConverter.GetBytes(storedRot[2]));
 
 					if (retainVel)
 					{
@@ -184,15 +183,9 @@ namespace DoomTrainer
 						process.WriteBytes(xVelPtr, BitConverter.GetBytes(0.01f));
 						process.WriteBytes(yVelPtr, BitConverter.GetBytes(0.01f));
 						process.WriteBytes(zVelPtr, BitConverter.GetBytes(1f));
-						//System.Threading.Thread.Sleep(100);
-						//process.WriteBytes(zVelPtr, BitConverter.GetBytes(0f));
-
-
 					}
-
 					process.WriteBytes(xLocPtr, BitConverter.GetBytes(storedPos[0]));
 					process.WriteBytes(yLocPtr, BitConverter.GetBytes(storedPos[1]));
-					//process.WriteBytes(zLocPtr, BitConverter.GetBytes(storedPos[2]+ 1f));
 					process.WriteBytes(zLocPtr, BitConverter.GetBytes(storedPos[2]));
 				}
 
@@ -210,63 +203,21 @@ namespace DoomTrainer
 
 
 
-		private bool GetPointers()
-		{
-			List<Process> processList = Process.GetProcesses().ToList().FindAll(x => x.ProcessName.Contains("DOOMEternalx64vk"));
-			if (processList.Count == 0)
-				return false;
-			process = processList[0];
-			if (process.HasExited)
-			{
-				process = null;
-				return false;
-			}
-
-			IntPtr raxIntPtr;
-			raxPointer.DerefOffsets(process, out raxIntPtr);
-			IntPtr eaxIntPtr;
-			eaxPointer.DerefOffsets(process, out eaxIntPtr);
-			int eaxValue = process.ReadValue<int>(eaxIntPtr);
-			eaxValue &= 0xFFFFFF;
-			eaxValue *= 0xB0;
-			IntPtr posPointer = new IntPtr(raxIntPtr.ToInt64() + eaxValue + 0x30);
-
-			
-
-			float xFloat;
-			process.ReadValue<float>(posPointer, out xFloat);
-
-			xLocPtr = posPointer;
-			yLocPtr = posPointer + 4;
-			zLocPtr = posPointer + 8;
-			
-			xVelDP.DerefOffsets(process, out xVelPtr);
-			yVelPtr = xVelPtr + 4;
-			zVelPtr = xVelPtr + 8;
-
-			rotDP.DerefOffsets(process, out xRotPtr);
-			yRotPtr = xRotPtr + 4;
-
-			yawDP.DerefOffsets(process, out xYawPtr);
-			yYawPtr = xYawPtr + 4;
-
-			frameTimeDebugDP.DerefOffsets(process, out frameTimeDebugPTR);
-
-
-			return true;
-		}
 
 		private void Button_Click(object sender, RoutedEventArgs e)
 		{
+			SigScanTarget consoleCmdScanTarget = new SigScanTarget("4C8B0EBA01000000488BCE448BF041FF51??8D");
+			SigScanTarget consoleBindScanTarget = new SigScanTarget("4C8B0FBA01000000488BCF448BF041FF51??4C6BC507");
+			IntPtr restrictedCommandsPtr;
+			IntPtr restrictedKeyPressPtr;
+
 			unlockButton.IsEnabled = false;
-			if (!GetPointers())
+			if (!hooked)
 			{
 				unlockButton.IsEnabled = true;
 				return;
 			}
-			
-
-			SignatureScanner sigScanner = new SignatureScanner(process, process.MainModuleWow64Safe().BaseAddress, process.MainModuleWow64Safe().ModuleMemorySize);
+			SignatureScanner sigScanner = new SignatureScanner(process, process.MainModule.BaseAddress, process.MainModule.ModuleMemorySize);
 			restrictedCommandsPtr = sigScanner.Scan(consoleCmdScanTarget);
 			restrictedKeyPressPtr = sigScanner.Scan(consoleBindScanTarget);
 			if (restrictedCommandsPtr.ToInt64() != 0 && restrictedKeyPressPtr.ToInt64() != 0)
@@ -278,10 +229,11 @@ namespace DoomTrainer
 				process.WriteBytes(restrictedCommandsPtr, BitConverter.GetBytes(0));
 				process.WriteBytes(restrictedKeyPressPtr, BitConverter.GetBytes(0));
 				System.Windows.Forms.MessageBox.Show("Console commands unlocked", "Success");
-			}else{
+			}
+			else
+			{
 				System.Windows.Forms.MessageBox.Show("Memory Address not found.\nEither console commands are already unlocked, or your game version is not supported.", "Error");
 			}
-
 			unlockButton.IsEnabled = true;
 		}
 
@@ -302,6 +254,142 @@ namespace DoomTrainer
 			retainRB.IsChecked = true;
 			retainVel = true;
 		}
+
+		public void ShowPos()
+		{
+			process.VirtualProtect(row1ptr, 1024, MemPageProtect.PAGE_READWRITE);
+			process.WriteBytes(row1ptr, ToByteArray("%i FPS", 8));
+
+			process.VirtualProtect(row2ptr, 1024, MemPageProtect.PAGE_READWRITE);
+			process.WriteBytes(row2ptr, ToByteArray("hvel: "+hVel.ToString("0.00") + " m/s", 79));
+
+			process.VirtualProtect(row3ptr, 1024, MemPageProtect.PAGE_READWRITE);
+			process.WriteBytes(row3ptr, ToByteArray("zvel: "+curZv.ToString("0.00") + " m/s", 19));
+
+			process.VirtualProtect(row4ptr, 1024, MemPageProtect.PAGE_READWRITE);
+			process.WriteBytes(row4ptr, ToByteArray("", 7));
+
+			process.VirtualProtect(row5ptr, 1024, MemPageProtect.PAGE_READWRITE);
+			process.WriteBytes(row5ptr, ToByteArray("", 34));
+
+			process.VirtualProtect(row9ptr, 1024, MemPageProtect.PAGE_READWRITE);
+			process.WriteBytes(row9ptr, ToByteArray("", 34));
+
+			process.VirtualProtect(row6ptr, 1024, MemPageProtect.PAGE_READWRITE);
+			process.WriteBytes(row6ptr, ToByteArray("pos: "+curX.ToString("0.00")+" "+curY.ToString("0.00")+" "+curZ.ToString("0.00"), 64));
+
+			process.VirtualProtect(row7ptr, 1024, MemPageProtect.PAGE_READWRITE);
+			process.WriteBytes(row7ptr, ToByteArray("vel: " + curXv.ToString("0.00") + " " + curYv.ToString("0.00") + " " + curZv.ToString("0.00"), 64));
+		}
+
+		public byte[] ToByteArray(string text, int length)
+		{
+			byte[] output = new byte[length];
+			byte[] textArray = Encoding.ASCII.GetBytes(text);
+			for (int i = 0; i < length; i++)
+			{
+				if (i >= textArray.Length)
+					break;
+				output[i] = textArray[i];
+			}
+			return output;
+		}
+
+		public bool Hook()
+		{
+			List<Process> processList = Process.GetProcesses().ToList().FindAll(x => x.ProcessName.Contains("DOOMEternalx64vk"));
+			if (processList.Count == 0)
+			{
+				process = null;
+				return false;
+			}
+			process = processList[0];
+			if (process.HasExited)
+				return false;
+			int mainModuleSize = process.MainModule.ModuleMemorySize;
+			SetPointersByModuleSize(mainModuleSize);
+			return true;
+		}
+
+		public void SetPointersByModuleSize(int moduleSize)
+		{
+			if (moduleSize == 507191296 || moduleSize == 515133440 || moduleSize == 510681088) // STEAM VERSION
+			{
+				Debug.WriteLine("Found Steam version");
+				raxDP = new DeepPointer("DOOMEternalx64vk.exe", 0x06121BB8, 0x38, 0x28, 0x0);
+				eaxDP = new DeepPointer("DOOMEternalx64vk.exe", 0x04C7CA08, 0xCB0, 0xDF8, 0x1D0, 0x88);
+				velDP = new DeepPointer("DOOMEternalx64vk.exe", 0x04C7CA08, 0x1510, 0x598, 0x1D0, 0x3F40);
+				rotDP = new DeepPointer("DOOMEternalx64vk.exe", 0x4C83F38);
+				yawDP = new DeepPointer("DOOMEternalx64vk.exe", 0x61AC728);
+				row1DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25B4A80);
+				row2DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25B4A88);
+				row3DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25B4AD8);
+				row4DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25B4AF0);
+				row5DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25B4AF8);
+				row6DP = new DeepPointer("DOOMEternalx64vk.exe", 0x5B83BC0);
+				row7DP = new DeepPointer("DOOMEternalx64vk.exe", 0x5B83244);
+				row8DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25B4B18);
+				row9DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25B4B28);
+			}
+			else if (moduleSize == 450445312 || moduleSize == 444944384) // BETHESDA VERSION
+			{
+				raxDP = new DeepPointer("DOOMEternalx64vk.exe", 0x060E38B8, 0x38, 0x28, 0x0);
+				eaxDP = new DeepPointer("DOOMEternalx64vk.exe", 0x04C3F008, 0xCB0, 0xDF8, 0x1D0, 0x88);
+				velDP = new DeepPointer("DOOMEternalx64vk.exe", 0x04C3F008, 0x1510, 0x598, 0x1D0, 0x3F40);
+				rotDP = new DeepPointer("DOOMEternalx64vk.exe", 0x4C46538);
+				yawDP = new DeepPointer("DOOMEternalx64vk.exe", 0x616E328);
+				row1DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25818C0);
+				row2DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25818C8);
+				row3DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25818C0 + 0x58);
+				row4DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25818C0 + 0x70);
+				row5DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25818C0 + 0x78);
+				row6DP = new DeepPointer("DOOMEternalx64vk.exe", 0x5B46140);
+				row7DP = new DeepPointer("DOOMEternalx64vk.exe", 0x5B457C4);
+				row8DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25818C0 + 0x98);
+				row9DP = new DeepPointer("DOOMEternalx64vk.exe", 0x25818C0 + 0xA8);
+			}
+			else //UNKNOWN GAME VERSION
+			{
+				System.Windows.Forms.MessageBox.Show("This game version is not supported.", "Unsupported Game Version");
+				process = null;
+			}
+		}
+
+		public void DerefPointers()
+		{
+			IntPtr raxIntPtr;
+			raxDP.DerefOffsets(process, out raxIntPtr);
+			IntPtr eaxIntPtr;
+			eaxDP.DerefOffsets(process, out eaxIntPtr);
+			int eaxValue = process.ReadValue<int>(eaxIntPtr);
+			eaxValue &= 0xFFFFFF;
+			eaxValue *= 0xB0;
+			IntPtr posPointer = new IntPtr(raxIntPtr.ToInt64() + eaxValue + 0x30);
+			xLocPtr = posPointer;
+			yLocPtr = posPointer + 4;
+			zLocPtr = posPointer + 8;
+
+			velDP.DerefOffsets(process, out xVelPtr);
+			yVelPtr = xVelPtr + 4;
+			zVelPtr = xVelPtr + 8;
+
+			rotDP.DerefOffsets(process, out xRotPtr);
+			yRotPtr = xRotPtr + 4;
+
+			yawDP.DerefOffsets(process, out xYawPtr);
+			yYawPtr = xYawPtr + 4;
+
+			row1DP.DerefOffsets(process, out row1ptr);
+			row2DP.DerefOffsets(process, out row2ptr);
+			row3DP.DerefOffsets(process, out row3ptr);
+			row4DP.DerefOffsets(process, out row4ptr);
+			row5DP.DerefOffsets(process, out row5ptr);
+			row6DP.DerefOffsets(process, out row6ptr);
+			row7DP.DerefOffsets(process, out row7ptr);
+			row8DP.DerefOffsets(process, out row8ptr);
+			row9DP.DerefOffsets(process, out row9ptr);
+		}
+
 	}
 }
 
--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-[Download latest release](https://github.com/Micrologist/DoomEternalTrainer/raw/master/bin/x64/Release/DoomTrainer.exe)
+[Download latest release](https://github.com/Micrologist/DoomEternalTrainer/raw/master/release/DoomEternalTrainer.exe)
